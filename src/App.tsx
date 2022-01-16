6235c4c42982f2b9f77d3498f04fba4ec7b0ff85
NGreenfield <NGreenfield@vt.edu>
2022-01-16 16:12:46 -0500

    Small Indentation corrections.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import Page from './components/page/Page'
 import './App.scss';
 
 function App() {
-  const [name, setName] = useState<string>('nik'); 
   const [color, setColor] = useState<string>('red'); 
-  //
+  
   return (
   <>
     <Page color="red" setColor={setColor}></Page>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,14 +7,14 @@ function Header({color, setColor}:any): JSX.Element {
         <div className={color}>
             <h1>Nikolas Greenfield</h1>
 
-                <ul>
-                    <li>
-                        <Link to="/home">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about-me">About Me</Link>
-                    </li>
-                </ul>
+            <ul>
+                <li>
+                    <Link to="/home">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about-me">About Me</Link>
+                </li>
+            </ul>
         </div>
     );
 }
