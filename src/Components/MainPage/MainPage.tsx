a4462a91221a620fad39152ac43c36578212f7ee
Nikolas Greenfield <Greenfieldnikolas@gmail.com>
2023-07-14 21:48:31 -0400

    Added resume text.

--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -66,18 +66,12 @@ function MainPage() {
                     </div>
                     <div id="BioText" style={BioTextStyle}>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                            labore et dolore magna aliqua. Bibendum neque egestas congue quisque. 
-                            Id porta nibh venenatis cras sed felis eget. Parturient montes nascetur 
-                            ridiculus mus mauris. Quis commodo odio aenean sed adipiscing diam donec. 
-                            Vel eros donec ac odio. Gravida arcu ac tortor dignissim convallis aenean 
-                            et. Leo in vitae turpis massa sed elementum. Urna neque viverra justo nec. 
-                            Sit amet nisl purus in. Consequat id porta nibh venenatis.
+                            Fresh graduate of Virginia Tech, software engineer, 
                         </p>
                     </div>
                 </div>
             </div>
-            <div style={pageContentStyle}>
+            <div id="ResumeContentBlock" style={pageContentStyle}>
                 <div id="PortfolioLinkSection">
                     <h1><Link to={'portfolio'} className="Link">
                         Check Out My Portfolio
@@ -85,12 +79,12 @@ function MainPage() {
                 </div>
 
                 <div id="ExperienceSection" className="SectionBlock">
-                    <h2>My Work Experience</h2>
+                    <h1 className="SectionHeader">My Experience</h1>
                     <div className="ResumeItems">
                         <h3>Software Engineering Internship - Parsons Corporation</h3>
-                        <h3 style={{marginLeft: "5vw"}}>May 2023 - Aug 2023</h3>
+                        <h3>May 2023 - Aug 2023</h3>
                     </div>
-                    <ul style={{listStyleType: "square"}}>
+                    <ul style={{listStyleType: "square", marginTop: "0"}}>
                         <li>
                             Developed data visualization dashboards, SharePoint list storage solutions, and pdf generators to provide creation, completion, and final storage of user applications utilizing Power Apps, Power Automate, OneDrive, and SharePoint.
                         </li>
@@ -101,40 +95,76 @@ function MainPage() {
                             Relied on cooperative development technologies including Jira iteration boards, Azure Dev-Ops, Slack communication, and Confluence documentation to ensure communication within and outside of the team.
                         </li>
                     </ul>
+                    <div className="ResumeItems">
+                        <h3>Capstone React Native Mobile App (Group Project)</h3>
+                        <h3>Spring 2023</h3>
+                    </div>
+                    <ul style={{listStyleType: "square", marginTop: "0"}}>
+                        <li>
+                            Developed a cross-platform React Native mobile application applying Google’s Maps and Trail APIs for an interactive map-oriented game which will educate children about nature while exploring through a scavenger hunt system with game scoring mechanics.                        </li>
+                        <li>
+                            Utilized Amazon Web Services (AWS) to create a RESTful API using Lambda Functions and the AWS Amplify library to interact with a DynamoDB No-SQL database to authenticate users and securely store user account and app information.
+                        </li>
+                        <li>
+                            Followed Agile methodologies such as sprints, standups, and sprint planning meetings to complete user stories managed with a Trello board and coordinated code version control using Git and GitHub.
+                        </li>
+                    </ul>
+                    <div className="ResumeItems">
+                        <h3>Machine Learning Course</h3>
+                        <h3>Spring 2023</h3>
+                    </div>
+                    <ul style={{listStyleType: "square", marginTop: "0"}}>
+                        <li>
+                            Learned major deep learning algorithms such as CNN and DNN, linear regression algorithms such as logistic regression and support vector machines, and decision trees, K-Nearest-Neighbors, and Naïve Bayes algorithms.                        </li>
+                        <li>
+                            Implemented multi-layer perceptron and CNN algorithms to identify handwritten digits in a final group project utilizing the Pytorch, NumPy, and Matplotlib python libraries.
+                        </li>
+                    </ul>
+                    <div className="ResumeItems">
+                        <h3>Vex Robotics Team Leader</h3>
+                        <h3>2012-2020</h3>
+                    </div>
+                    <ul style={{listStyleType: "square", marginTop: "0"}}>
+                        <li>
+                            As team leader, I lead teammates in designing, building, and programming robots and directed team responsibilities and time-management to compete in competition.
+                        </li>
+                        <li>
+                            Competed in the Vex Robotics 2018 & 2019 State Championships.
+                        </li>
+                        <li>
+                            Learned how to code effectively in the C-based “Robot C” programming language.
+                        </li>
+                    </ul>
                 </div>
                 <div  id="EducationSection" className="SectionBlock">
-                    <h2 style={{textAlign: "right"}}>My Education</h2>
-                    <table>
-                        <tr>
-                            <td style={{width: "60%"}}>
-                                <h3>Virginia Tech</h3>
-                                <h3>Bachelor of Science in Computer Science</h3>
-                            </td>
-                            <td style={{width: "40%", border: "1px solid red"}}>
-                                <h3>Graduated: May 2023</h3>
-                                <h3>GPA: 3.66</h3>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style={{width: "60%"}}>
-                                <h3>Germanna Scholars Student</h3>
-                            </td>
-                            <td style={{width: "40%"}}>
-                                <h3>Graduated: May 2020</h3>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style={{width: "60%"}}>
-                                <h3>General Studies associate degree</h3>
-                            </td>
-                            <td style={{width: "40%"}}>
-                                <h3>GPA: 4.0</h3>
-                            </td>
-                        </tr>
-                    </table>
+                    <h1 className="SectionHeader" style={{textAlign: "center"}}>My Education</h1>
+                    <div className="ResumeItems ShortenedWidth">
+                        <h3 className="NoMP">Virginia Tech</h3>
+                        <h3 className="NoMP">Graduated: May 2023</h3>
+                    </div>
+                    <div className="ResumeItems ShortenedWidth">
+                        <h3 className="NoMP">Bachelor of Science in Computer Science</h3>
+                        <h3 className="NoMP">GPA: 3.66</h3>
+                    </div>
+                    <div className="ResumeItems ShortenedWidth">
+                        <h3 className="NoMP" style={{fontSize: "0.9em"}}>Beyond Boundaries Scholarship</h3>
+                        <h3 className="NoMP" style={{fontSize: "0.9em"}}>2020-2023</h3>
+                    </div>
+                    <div className="ResumeItems ShortenedWidth">
+                        <h3 className="NoMP" style={{marginTop: "2em"}}>Germanna Scholars Student</h3>
+                        <h3 className="NoMP" style={{marginTop: "2em"}}>Graduated: May 2020</h3>
+                    </div>
+                    <div className="ResumeItems ShortenedWidth">
+                        <h3 className="NoMP">General Studies associate degree</h3>
+                        <h3 className="NoMP">GPA: 4.0</h3>
+                    </div>
+                    <div className="ResumeItems ShortenedWidth">
+                        <h3 className="NoMP" style={{fontSize: "0.9em"}}>President's List</h3>
+                        <h3 className="NoMP" style={{fontSize: "0.9em"}}>2018-2020</h3>
+                    </div>
                 </div>
                 <div  id="SkillsSection" className="SectionBlock">
-                    <h2>My Skills</h2>
+                    <h1 className="SectionHeader" style={{textAlign: "right"}}>My Skills</h1>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
                         labore et dolore magna aliqua. Bibendum neque egestas congue quisque. 
